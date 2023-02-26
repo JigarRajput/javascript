e34bc1391fe657f94802acb0525c81ddc555db1e
Jigar Subhash Rajput <80737219+JigarRajput@users.noreply.github.com>
2023-02-27 00:20:43 +0530

    Create arrayMethods.js

---
+++ arrayMethods.js
@@ -0,0 +1,33 @@
+// jai Ganesh
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// reduce method without default value on empty array throws error 
+// let timesExecuted = 0
+// const retVal = array.reduce((accumulator, currentValue) => {
+//     console.log(accumulator, currentValue, ++timesExecuted)
+//     return accumulator + currentValue
+// })
+
+// reduce method with default value on empty array does not throws error 
+
+// const retValue = array.reduce((accumulator, currentValue) => {
+//     console.log(accumulator, currentValue)
+//     return accumulator + currentValue
+// }, 10)
+
+console.log(retValue)
+
+// reduce method with current Index
+
+// let returnValue = array.reduce((accumulator, currentValue, currentIndex) => {
+//     console.log(accumulator, currentValue, currentIndex)
+//     return accumulator + currentValue
+// })
+
+// let returnValue = array.reduce((accumulator, currentValue, currentIndex, array) => {
+//     console.log(accumulator, currentValue, currentIndex, array)
+//     return accumulator + currentValue
+// })
+
+
